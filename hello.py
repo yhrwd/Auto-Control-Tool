a38f8f81859eaaad327d9f6a3f6b3d1d130d261e
YHRWD <yhrza@outlook.com>
2025-03-25 23:54:03 +0800

    mousepos display

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
-from scdow import windows as wsc
+from scdow import windows as wsc, mouseControl as msc
 
-winsel = wsc.wininit("文件",0,0,800,500)
+winsel = wsc.wininit("文件", 0, 0, 800, 500)
+msc.mousePos()
---
+++ scdow/mouseControl.py
@@ -0,0 +1,12 @@
+import pyautogui
+
+def mousePos():
+    print('输入 Ctrl-C 退出.')
+    try:
+        while True:
+            x, y = pyautogui.position()
+            positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+            print(positionStr, end='')
+            print('\b' * len(positionStr), end='', flush=True)
+    except KeyboardInterrupt:
+        print('\n')
